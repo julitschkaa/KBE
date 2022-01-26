52a42074032eeb728e74f67ab18951126d97bd67
julitschkaa <schaak@posteo.net>
2022-01-26 22:06:01 +0100

    fixed tax_calculator import in views

--- tax_calc/views.py
+++ tax_calc/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer, TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
-from tax_calculator import calculate_tax
+from .tax_calculator import calculate_tax
 
 @api_view()#by default only get requests are allowed
 @renderer_classes([TemplateHTMLRenderer,JSONRenderer])
