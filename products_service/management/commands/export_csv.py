3fcf260dc290db850d26d541f5449a4bf6d7f39b
julitschkaa <schaak@posteo.net>
2022-02-03 21:35:52 +0100

    made csvworker upload products.csv to sftp server

--- products_service/management/commands/export_csv.py
+++ products_service/management/commands/export_csv.py
@@ -1,6 +1,8 @@
+import os
 from django.core.management.base import BaseCommand, CommandError
 from products_service.models import Product
 import csv
+import pysftp
 
 class Command(BaseCommand):
     help = 'export all data in csv format'
@@ -13,4 +15,14 @@ class Command(BaseCommand):
             writer = csv.writer(f)
             writer.writerows(products)
 
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None #not advised for production, really not.
+        srv = pysftp.Connection(host=os.environ.get("SFTP_HOST", ""), username=os.environ.get("SFTP_USERNAME", ""),
+                                password=os.environ.get("SFTP_PASSWORD", ""), cnopts=cnopts) #log="./temp/pysftp.log"
+
+        with srv.cd('upload'):  # chdir to public
+            srv.put('products.csv')  # upload file to sftpserver
+
+        # Closes the connection
+        srv.close()
         self.stdout.write(self.style.SUCCESS('Successfully exported csv'))
