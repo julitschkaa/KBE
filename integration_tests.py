9895dd7a3aec674a746dfe9d668347018a8b0750
julitschkaa <schaak@posteo.net>
2022-02-08 19:55:51 +0100

    refined integration test script

--- integration_tests.py
+++ integration_tests.py
@@ -1,5 +1,9 @@
+import os
+
 import requests
 import requests.exceptions
+from pymongo import MongoClient
+
 
 def test_calculate_tax_with_100_cents():
     tax = None
@@ -13,3 +17,17 @@ def test_check_size_of_API_produclist_is_10():
     assert r.status_code == 200
     count = r.json()["count"]
     assert count == 10
+
+def test_compare_size_of_API_produclist_with_mangodb2():
+    r = requests.get("http://localhost/products/",headers={"accept": "application/json"})
+    assert r.status_code == 200
+    count = r.json()["count"]
+    # open connection to mangodb2
+    client = MongoClient("localhost", port=8081, username=os.environ.get("MANGODB2_USERNAME"),
+                         password=os.environ.get("MANGODB2_PASSWORD"), authMechanism='SCRAM-SHA-256')
+    db = client["mangodb2"]
+    # check if a collection named products already exists
+    collectionnames = db.list_collection_names()
+    assert "products" in collectionnames
+    mangocount = db.products.count_documents({})
+    assert count == mangocount
--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
+source .env
 poetry install
 source `poetry env info --path`/bin/activate
 echo "---starting unit tests of tax-calc---"
 pytest tax_calc/tax_calculator_unit_tests.py
 echo "---starting integration Tests---"
+export MANGODB2_USERNAME=$MANGO2_INITDB_ROOT_USERNAME
+export MANGODB2_PASSWORD=$MANGO2_INITDB_ROOT_PASSWORD
 pytest integration_tests.py
